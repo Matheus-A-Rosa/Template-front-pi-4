6b38460a1a59f3810189a013af62859f24214d32
Matheus Rosa <matheus_a_rosa@hotmail.com>
2021-10-18 19:05:14 -0300

    definido estilos globais

--- pi4/src/App.js
+++ pi4/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import global from './styles/global.css'
 
 import Routes from './components/Routes'
 
-const App = () => <Routes />;
+const App = () => 
+<global>
+<Routes />;
+</global>
 
 
 export default App
